4dc27564a5bd709f7ac2e62cc3ad5a29c87c8660
Charlie Bini <cbini@kippnj.org>
2017-06-06 21:35:48 +0000

    on second thought... keep pyvirtualdisplay

--- sync_steptool.py
+++ sync_steptool.py
@@ -7,11 +7,11 @@ import re
 from io import StringIO
 import pandas as pd
 from gcloud import storage
+from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 
 STEP_USERNAME = CONFIG['step_username']
 STEP_PASSWORD = CONFIG['step_password']
@@ -44,77 +44,79 @@ def main():
     selenium on PythonAnywhere is a bugged older version
     so every `find_element` call is wrapped in a try/except block
     """
-    print('Starting webdriver...')
-    driver = webdriver.Firefox()
-
-    try:
-        driver.get("https://www.steptool.org/account/login/?next=/home/")
-
-        ## login
-        print('Logging in...')
-        try:
-            element = driver.find_element_by_id('id_username')
-        except:
-            element = driver.find_element_by_id('id_username')
-        finally:
-            element.send_keys(STEP_USERNAME)
+    with Display():
+        print('Starting webdriver...')
+        driver = webdriver.Firefox()
 
         try:
-            element = driver.find_element_by_id('id_password')
-        except:
-            element = driver.find_element_by_id('id_password')
+            driver.get("https://www.steptool.org/account/login/?next=/home/")
+
+            ## login
+            print('Logging in...')
+            try:
+                element = driver.find_element_by_id('id_username')
+            except:
+                element = driver.find_element_by_id('id_username')
+            finally:
+                element.send_keys(STEP_USERNAME)
+
+            try:
+                element = driver.find_element_by_id('id_password')
+            except:
+                element = driver.find_element_by_id('id_password')
+            finally:
+                element.send_keys(STEP_PASSWORD)
+
+            element.send_keys(Keys.ENTER)
+
+            ## navigate to Data Exports
+            print('Navigating to Data Exports...')
+            WebDriverWait(driver, 10).until(
+                EC.title_is('STEP: My Home Page')
+            )
+            try:
+                element = driver.find_element_by_link_text('Data Exports')
+            except:
+                element = driver.find_element_by_link_text('Data Exports')
+            finally:
+                element.click()
+
+            ## navigate to export page
+            print('Navigating to STEP Level Assessment export...')
+            WebDriverWait(driver, 10).until(
+                EC.title_is('{} Schools Exports'.format(DISTRICT_NAME_FULL))
+            )
+            try:
+                element = driver.find_element_by_link_text('STEP Level Assessment Data')
+            except:
+                element = driver.find_element_by_link_text('STEP Level Assessment Data')
+            finally:
+                element.click()
+
+            ## navigate to report download page
+            print('Exporting session data...')
+            WebDriverWait(driver, 10).until(
+                EC.title_is('Export Step Level Assessment Data')
+            )
+
+            ## export cookies from Selenium to pass along to requests session
+            current_url = driver.current_url
+            export_url = current_url.replace('step_level.html','step_all.csv')
+            all_cookies = driver.get_cookies()
+
         finally:
-            element.send_keys(STEP_PASSWORD)
+            driver.quit()
+            print('Quitting webdriver...')
 
-        element.send_keys(Keys.ENTER)
-
-        ## navigate to Data Exports
-        print('Navigating to Data Exports...')
-        WebDriverWait(driver, 10).until(
-            EC.title_is('STEP: My Home Page')
-        )
-        try:
-            element = driver.find_element_by_link_text('Data Exports')
-        except:
-            element = driver.find_element_by_link_text('Data Exports')
-        finally:
-            element.click()
-
-        ## navigate to export page
-        print('Navigating to STEP Level Assessment export...')
-        WebDriverWait(driver, 10).until(
-            EC.title_is('{} Schools Exports'.format(DISTRICT_NAME_FULL))
-        )
-        try:
-            element = driver.find_element_by_link_text('STEP Level Assessment Data')
-        except:
-            element = driver.find_element_by_link_text('STEP Level Assessment Data')
-        finally:
-            element.click()
-
-        ## navigate to report download page
-        print('Exporting session data...')
-        WebDriverWait(driver, 10).until(
-            EC.title_is('Export Step Level Assessment Data')
-        )
-
-        ## export cookies from Selenium to pass along to requests session
-        current_url = driver.current_url
-        export_url = current_url.replace('step_level.html','step_all.csv')
-        all_cookies = driver.get_cookies()
-    finally:
-        driver.quit()
-
-    print('Quitting webdriver...')
-
+    print('Exporting cookies...')
     session_cookies = {}
     for s_cookie in all_cookies:
         session_cookies[s_cookie['name']] = s_cookie['value']
 
     ## switch over to requests to export the data files
     print('Downloading...')
-    s = requests.Session()
-    r = s.get(export_url, cookies=session_cookies)
+    with requests.Session() as s:
+        r = s.get(export_url, cookies=session_cookies)
 
     ## parse save file variables
     r_contentdisposition = r.headers['Content-Disposition']
